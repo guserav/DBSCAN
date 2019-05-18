3a8b4dd20d7e059ca30854dfde6d6606b5be2265
guserav <erik.zeiske@web.de>
2019-05-18 11:23:54 -0700

    Add argparser for easier handling of visualisation

--- visualizeRtree.py
+++ visualizeRtree.py
@@ -1,22 +1,33 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import re
+import argparse
+import sys
 
-file = open("../output.data", "r")
+parser = argparse.ArgumentParser()
+parser.add_argument("input", nargs='?', type=argparse.FileType('r'), default=sys.stdin)
+parser.add_argument("output", nargs='?', type=str, default="plot.png")
+parser.add_argument("-d", "--delim", nargs='?', help="Delimiter to use for splitting point coordinates", default=";")
+parser.add_argument("-r", "--split", nargs='?', help="(Regex) to use for splitting several points an depth", default=",|:")
+parser.add_argument("-s", "--strip", nargs='?', help="Strip these characters away from points on both sides", default="[]\n")
+parser.add_argument("--dpi", type=int, default=2400)
+args = parser.parse_args()
+
 colors = ['b', 'g', 'r', 'c', 'm', 'y']
-reg = re.compile(",|:")
+reg = re.compile(args.split)
 x = []
 y = []
 plt.figure(dpi=300)
-for line in list(file)[::-1]:
+for line in list(args.input)[::1]:
     lineSplit = reg.split(line)
     if(len(lineSplit) == 2):
-        split = lineSplit[1].strip("[]\n").split(";")
+        split = lineSplit[1].strip(args.strip).split(args.delim)
+        #if(float(split[0]) < 200 and float(split[0]) > 100 and float(split[1]) < 200 and float(split[1]) > 100):
         x.append(float(split[0]))
         y.append(float(split[1]))
     else:
-        split1 = lineSplit[1].strip("[]\n").split(";")
-        split2 = lineSplit[2].strip("[]\n").split(";")
+        split1 = lineSplit[1].strip(args.strip).split(args.delim)
+        split2 = lineSplit[2].strip(args.strip).split(args.delim)
         split1 = list(map(float, split1))
         split2 = list(map(float, split2))
         a = [split1[0],
@@ -30,6 +41,7 @@ for line in list(file)[::-1]:
             split2[1],
             split1[1]]
         depth = int(lineSplit[0])
-        if depth < 4:
+        if depth > 1:
             plt.plot(a, b, colors[depth], linewidth=1/(2*depth+1))
-plt.show()
+plt.plot(x, y, 'k.-',linewidth=0, markersize=1/5, markeredgewidth=1/5)
+plt.savefig('plot.png', dpi=args.dpi)
