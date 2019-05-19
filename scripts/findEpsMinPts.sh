0f26be937fa54fad3608f8e612aa9534a4f284fc
guserav <erik.zeiske@web.de>
2019-05-18 19:22:57 -0700

    Add rough concatenation of the tiles
    This is only for documentation as * messes up the order of the images.

--- scripts/findEpsMinPts.sh
+++ scripts/findEpsMinPts.sh
@@ -5,6 +5,8 @@ do
     for minPts in $(seq $7 $8 $9)
     do
         echo "Now doing $eps : $minPts"
-        build/StudienarbeitCode $1 $2 $3 $eps $minPts | python3 scripts/visualizeCluster.py -o "scripts/out/plot${eps}_$minPts.png"
+        build/StudienarbeitCode $1 $2 $3 $eps $minPts | python3 scripts/visualizeCluster.py -o "scripts/out/plot${eps}_$minPts.png" --dpi 150
     done
+    convert scripts/out/plot${eps}_*.png +append scripts/out/line$eps.png
 done
+convert scripts/out/line*.png -append scripts/out/output.png
