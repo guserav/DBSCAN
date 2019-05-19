c467df3fc9618028461ed4d5a8d6e9da7b340327
guserav <erik.zeiske@web.de>
2019-05-18 17:14:14 -0700

    Add small script to look at what eps and minPts do

---
+++ scripts/findEpsMinPts.sh
@@ -0,0 +1,10 @@
+#! bash
+
+for eps in $(seq $4 $5 $6)
+do
+    for minPts in $(seq $7 $8 $9)
+    do
+        echo "Now doing $eps : $minPts"
+        build/StudienarbeitCode $1 $2 $3 $eps $minPts | python3 scripts/visualizeCluster.py -o "scripts/out/plot${eps}_$minPts.png"
+    done
+done
