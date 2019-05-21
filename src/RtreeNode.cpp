342a62ca04d57ab023f7fa917214ead817be0446
guserav <erik.zeiske@web.de>
2019-05-20 17:04:45 -0700

    Fix over counting by one

--- src/RtreeNode.cpp
+++ src/RtreeNode.cpp
@@ -569,7 +569,7 @@ float RtreeNode::calculateVolume(RtreeNode *allChilds[R_TREE_NUMBER_CHILDS + 1],
     float volume2 = 1.0f;
     //boundaries of the current dimensions (1: [0, k], 2: [k+1, R_TREE_NUMBER_CHILDS])
     float s1, s2, e1, e2;
-    for(int i=0; i <= dimensions; i++) { //TODO in case of i == d the finding of minDim can be optimised...
+    for(int i=0; i < dimensions; i++) { //TODO in case of i == d the finding of minDim can be optimised...
         //First determine the boundaries of the two rectangles
         s1 = allChilds[0]->minBoundaries[i];
         e1 = allChilds[0]->maxBoundaries[i];
