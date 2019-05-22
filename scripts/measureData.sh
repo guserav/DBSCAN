a62dcef8d221c93ef42610d05a8a4227a78e2ef7
guserav <erik.zeiske@web.de>
2019-05-22 16:08:35 -0700

    Add status report for convenience on stderr

--- scripts/measureData.sh
+++ scripts/measureData.sh
@@ -13,14 +13,17 @@ set OMP_DYNAMIC=0
 data=(blobs circles noise)
 eps=("0.1" "0.1" "0.2")
 minPts=(5 5 2)
+echo "program=${program} start=${sta} step=${step} end=${end} runs=${runs} task_name=${task_name}" >&2
 
 for i in ${!data[*]}
 do
     if [ "$mode" = "single" ]; then
+        echo "Doing single ${data[$i]}" >&2
         $program "${task_name}\$${data[$i]}\$0" ";" $runs "data/${data[$i]}.data" 2 ";" "${eps[$i]}" "${minPts[$i]}" 2>&1 > /dev/null
     else
         for thread_count in $(seq $sta $end $step)
         do
+            echo "Doing ${data[$i]}\$${thread_count}" >&2
             set OMP_NUM_THREADS=$thread_count
             $program "${task_name}\$${data[$i]}\$${thread_count}" ";" $runs "data/${data[$i]}.data" 2 ";" "${eps[$i]}" "${minPts[$i]}" 2>&1 > /dev/null
         done
